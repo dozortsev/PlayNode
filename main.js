4e4f7d21dde2d493d9ae24f096f0e4a6ecb07945
dozortsev <dozortsevanton@gmail.com>
2014-11-04 23:51:27 +0200

    Init commit

---
+++ main.js
@@ -0,0 +1,14 @@
+var fs = require('fs'),
+	path = require('path');
+
+// fs.readdir('.', function (err, files) {
+// 	files
+// 	.filter(function (file) {
+// 		return path.extname(file) === '.txt'
+// 	})
+// 	.forEach(function (file) {
+// 		fs.renameSync(file, )
+// 	});
+// });
+
+console.log(process.cwd());
