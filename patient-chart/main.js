84b64bb1559cd4a86fe58ce5d3c60b01ecf35e3a
Anton <dozortsevanton@gmail.com>
2015-09-12 22:56:38 +0300

    Add use cases with tooltips

--- patient-chart/main.js
+++ patient-chart/main.js
@@ -1,5 +1,6 @@
 var app = angular.module('patient-chart', ['ui.bootstrap']);
 
-app.controller('MainCtrl', [function () {
-    console.log('main controller');
+app.controller('MainCtrl', ['$scope', function ($scope) {
+
+    $scope.tooltipContent = 'Lorem ipsum dolor sit amet.\n Lorem ipsum dolor.';
 }]);
