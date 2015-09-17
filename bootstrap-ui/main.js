37452e73704883c79a8494c6d7c5616215aa9796
Anton <dozortsevanton@gmail.com>
2015-09-17 22:44:09 +0300

    Fix #3: Add example with datapicker on page and inside in modal

--- bootstrap-ui/main.js
+++ bootstrap-ui/main.js
@@ -1,12 +1,48 @@
 var app = angular.module('patient-chart', ['ui.bootstrap']);
 
-app.controller('MainCtrl', ['$scope', function ($scope) {
+app.controller('MainCtrl', ['$scope', '$modal', function ($scope, $modal) {
 
     $scope.tooltipContent = 'Lorem ipsum dolor sit amet.\n Lorem ipsum dolor.' +
         'Lorem ipsum dolor sit amet.\n Lorem ipsum dolor.' +
         'Lorem ipsum dolor sit amet.\n Lorem ipsum dolor.' +
         'Lorem ipsum dolor sit amet.\n Lorem ipsum dolor.';
+
+    $scope.datePicker = {
+        dt: new Date(),
+        opened: false,
+
+        open: function () {
+            this.opened = true;
+        }
+    };
+
+    $scope.openModal = function () {
+        var niceModal = $modal.open({
+            size: 'lg',
+            animation: true,
+            templateUrl: 'nice-modal',
+            controller: 'ModalCtrl'
+        });
+    }
 }]);
+
+app.controller('ModalCtrl', ['$scope', '$modalInstance', ModalCtrl]);
+
+function ModalCtrl($scope, $modalInstance) {
+
+    $scope.datePicker = {
+        dt: new Date(),
+        opened: false,
+
+        open: function () {
+            this.opened = true;
+        }
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss();
+    };
+};
 
 
 app.controller('TooltipCtrl', ['$scope', function ($scope) {
