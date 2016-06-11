02b1a39c16b4f4765485f5a54824133bfd8e8df3
Anton <dozortsevanton@gmail.com>
2016-06-11 22:50:45 +0300

    Enable code coverage plugin for JS code

--- angularjs-up-and-running/exercise3/karma.conf.js
+++ angularjs-up-and-running/exercise3/karma.conf.js
@@ -29,13 +29,18 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'app/**/*.js': ['coverage']
     },
 
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
@@ -62,10 +67,11 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity
-  })
-}
+  });
+};
+
--- coffee/README.md
+++ coffee/README.md
@@ -6,6 +6,7 @@ Books:
 
 Other topics, links and useful resources:
 
+* **[Welcome to the CoffeeScript Cookbook!](https://coffeescript-cookbook.github.io/)**
 * [AngularJS CoffeeScript Style Guide](https://github.com/Plateful/plateful-mobile/wiki/AngularJS-CoffeeScript-Style-Guide)
 * [Course about CoffeeScript by Zorax](https://www.youtube.com/playlist?list=PLwSSV-_L9sztYcaMbY2XlehMFeR8Khs0j)
 * ~~[Better JS with CoffeeScript](https://vimeo.com/35258313)~~
